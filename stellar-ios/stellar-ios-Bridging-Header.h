2e816409bb2d33c7a877f61bb62e723b84451259
J12D <heinzderalligator@googlemail.com>
2016-01-31 12:22:10 +0100

    Reference AFNetworking root header

--- stellar-ios/stellar-ios-Bridging-Header.h
+++ stellar-ios/stellar-ios-Bridging-Header.h
@@ -2,5 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <AFNetworking/AFHTTPRequestOperation.h>
-#import <AFNetworking/AFHTTPRequestOperationManager.h>
+#import <AFNetworking/AFNetworking.h>
+//#import <AFNetworking/AFHTTPRequestOperation.h>
+//#import <AFNetworking/AFHTTPRequestOperationManager.h>
